f4ef263d15a3a47d9ba4a18f2e95620f539cce84
Shiv Gupta <shivg56@gmail.com>
2023-04-01 17:36:14 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import { useState } from 'react';
-import './App.css';
-import { ATTRIBUTE_LIST, CLASS_LIST, SKILL_LIST } from './consts.js';
-
+import { useState } from "react";
+import "./App.css";
+import { ATTRIBUTE_LIST, CLASS_LIST, SKILL_LIST } from "./consts.js";
 
 function App() {
   const [num, setNum] = useState(0);
   return (
     <div className="App">
       <header className="App-header">
-        <h1>React Coding Exercise</h1>
+        <h1>React Coding Exercise by Shiv</h1>
       </header>
       <section className="App-section">
         <div>
